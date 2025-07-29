6a47e4cb2d1d8c3d99bd7f30c3141756115b16cb
ganeshk <ganeshkannan627@gmail.com>
2025-07-29 14:14:54 +0530

    test: add duplicate test for 'get started' link functionality

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,13 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started heading.
+  await page.getByRole('heading', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
