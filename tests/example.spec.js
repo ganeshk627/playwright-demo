da2823de5aedaf5519d7776541efc3a16f535a70
ganeshk <ganeshkannan627@gmail.com>
2025-07-29 14:15:42 +0530

    fix: update test to click the 'get started' link instead of heading

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -21,8 +21,8 @@ test('get started link', async ({ page }) => {
 test('test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Click the get started heading.
-  await page.getByRole('heading', { name: 'Get started' }).click();
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
